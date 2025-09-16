6ffc53e9c9bcff2ffcb8b121acc61fa08be74730
Arun <kumararungobi@gmail.com>
2025-09-16 12:00:18 +0530

    modified

---
+++ src/pages/Booking.tsx
@@ -0,0 +1,13 @@
+import BookingForm from "@/components/ui/booking-form";
+import Navigation from "@/components/ui/navigation";
+
+const Booking = () => {
+  return (
+    <div className="min-h-screen">
+      <Navigation />
+      <BookingForm />
+    </div>
+  );
+};
+
+export default Booking;
